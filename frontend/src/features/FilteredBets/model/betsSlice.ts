afce356cb4733fdebff2d8d18395372eda4c4b88
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 01:42:11 +0300

    feat: add betsSlice

---
+++ frontend/src/features/FilteredBets/model/betsSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import {  StatusFilter } from "./types"
+
+const initialState = {
+  statusFilter: "All",
+}
+
+const betsSlice = createSlice({
+  name: "bets",
+  initialState,
+  reducers: {
+    setStatusFilter(state, action: PayloadAction<StatusFilter>) {
+      state.statusFilter = action.payload
+    },
+  },
+})
+
+export const { setStatusFilter } = betsSlice.actions
+export default betsSlice
