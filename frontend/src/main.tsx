cea528660946b91e9ada6c3ae50dbaf5b60ddaf1
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 01:41:15 +0300

    style: add global styles to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import { store } from './app/store/store.ts'
 import { Provider } from 'react-redux'
+import { GlobalStyles } from './app/styles/GlobalStyles.ts'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-      <StrictMode>
+    <StrictMode>
+          <GlobalStyles />
       <App />
     </StrictMode>,
   </Provider>
