4fca642a4a4091a7e53076bdc7ab1be7931e82b7
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 01:44:11 +0300

    feat: update store

--- frontend/src/app/store/store.ts
+++ frontend/src/app/store/store.ts
@@ -1,11 +1,13 @@
-
+import { betsApi } from "@/features/FilteredBets/api/betsApi"
+import betsSlice from "@/features/FilteredBets/model/betsSlice"
 import { configureStore } from "@reduxjs/toolkit"
 
 export const store = configureStore({
   reducer: {
-   
+    [betsApi.reducerPath]: betsApi.reducer,
+    [betsSlice.name]: betsSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(betsApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
