d9490c2b4bf7d266c9ee17b6673f8389cf6f0d99
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 01:28:20 +0300

    chore: add redux provider to root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
+import { store } from './app/store/store.ts'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+      <StrictMode>
+      <App />
+    </StrictMode>,
+  </Provider>
+ 
 )
