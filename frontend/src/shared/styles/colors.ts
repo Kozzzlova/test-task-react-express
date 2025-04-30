6464fecae0a4a9e75ff3e4df57cf756fe37bf5ca
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 01:37:06 +0300

    style: add colors var

---
+++ frontend/src/shared/styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#716FAF",
+  secondary: "#395389",
+  accent: "#FFFFFF",
+  black: "#000000",
+  statusPending: "#f2f573",
+  statusWon: "#83dd5f",
+  statusLost: "#d51e2d",
+  buttonActive: "#1d273b",
+}
