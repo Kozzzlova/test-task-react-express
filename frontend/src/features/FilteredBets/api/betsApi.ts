50255dd71835cdb6029b8cfec919f33ebe9797b5
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 01:42:00 +0300

    feat: add betsApi

---
+++ frontend/src/features/FilteredBets/api/betsApi.ts
@@ -0,0 +1,22 @@
+// betsApi.ts
+import { baseAppApi } from "@/app/services/baseAppApi";
+import { Bet, BetsStatus } from "../model/types"
+
+export const betsApi = baseAppApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getBets: builder.query<Bet[], void>({
+      query: () => "bets",
+      providesTags: ["Bets"],
+    }),
+    updateBet: builder.mutation<Bet, { betId: number; status: BetsStatus }>({
+      query: ({ betId, status }) => ({
+        url: `bets/${betId}`,
+        method: "PUT",
+        body: { status },
+      }),
+      invalidatesTags: ["Bets"],
+    }),
+  }),
+})
+
+export const { useGetBetsQuery, useUpdateBetMutation } = betsApi
