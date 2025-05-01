a90fc7106c412b23e197610a753b4e1f5f4aa681
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 12:48:08 +0300

    refactor:  rename PORT to port

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const betService = require('./bll/betService');
 const { seedBets } = require('./dal/betRepository');
