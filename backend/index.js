8762f3ef785ffb49a6b4d88c218498b53b67b99b
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 12:41:21 +0300

    chore: add dotenv dependency

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 const betService = require('./bll/betService');
 const { seedBets } = require('./dal/betRepository');
