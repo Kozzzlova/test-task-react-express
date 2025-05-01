17212df2cd48fd8f730c77c2f72c703bbffc0a27
Kozzzlova <kozlova.jully@gmail.com>
2025-05-01 13:13:26 +0300

    refactor: use env var in baseUrl

--- frontend/src/app/services/baseAppApi.ts
+++ frontend/src/app/services/baseAppApi.ts
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-
+export const API_URL = import.meta.env.VITE_API_URL;
 export const baseAppApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: API_URL,
   }),
   tagTypes: ["Bets"],
   endpoints: () => ({}),
